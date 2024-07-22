cf3f296c3b9c2d2173886215da550e9cc7b54775
rhnlf_ <alif.satoe@gmail.com>
2024-07-22 15:58:22 +0700

    chore: Refactor profile page to handle edit and delete actions

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation"
 import Profile from "@components/Profile"
 
 const MyProfile = () => {
+    const router = useRouter()
     const { data: session } = useSession()
 
     const [posts, setPosts] = useState([])
@@ -23,12 +24,26 @@ const MyProfile = () => {
     }, [])
 
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
 
+        if(hasConfirmed) {
+            try {
+                const response = await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                })
+
+                if(response.ok) {
+                    setPosts(posts.filter(p => p._id !== post._id))
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
 
     return (
